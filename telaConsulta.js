583b6807b6616f301094d8ec5647b94ed91b6991
NickolasKnihs <160547042+NickolasKnihs@users.noreply.github.com>
2024-04-25 19:55:21 -0300

    Add files via upload

--- telaConsulta.js
+++ telaConsulta.js
@@ -1,13 +1,12 @@
-
 let pessoas = JSON.parse(localStorage.getItem("pessoasCadastro"));
 if (!pessoas) {
     pessoas = [];
 }
-for (const pessoa of pessoas) {
-    mostraPessoa(pessoa);
+for (let i = 0; i < pessoas.length; i++) {
+    mostraPessoa(pessoas[i], i); // Adicionando o índice como segundo parâmetro
 }
 
-function mostraPessoa(parametro){
+function mostraPessoa(parametro, indice) { // Recebendo também o índice
     const paragrafo = document.createElement('p');
     paragrafo.innerHTML += `
         <span> Nome: ${parametro.nome}</span>
@@ -22,12 +21,25 @@ function mostraPessoa(parametro){
         <br>
         <span> E-mail: ${parametro.email}</span>
         <br>
-        <span> Genero: ${parametro.genero}</span>
+        <span> Gênero: ${parametro.genero}</span>
         <br>
         <div class="buttons-container">
-        <button onclick="window.location.href = './telaConsulta.html';" target="_blank">REMOVER</button>
-        <button onclick="window.location.href = './telaApresentacao.html';" target="_blank">EDITAR</button>
+            <button onclick="removerUsuario(${indice});">REMOVER</button> <!-- Chama a função removerUsuario passando o índice -->
+            <button onclick="window.location.href = './telaApresentacao.html';" target="_blank">EDITAR</button>
         </div>
     `;
     tabelaCadastro.append(paragrafo);
+}
+
+function removerUsuario(indice) {
+    pessoas.splice(indice, 1); // Remove o usuário da matriz pelo índice
+    localStorage.setItem("pessoasCadastro", JSON.stringify(pessoas)); // Atualiza o localStorage
+    // Atualiza a exibição removendo todos os elementos filhos de tabelaCadastro
+    while (tabelaCadastro.firstChild) {
+        tabelaCadastro.removeChild(tabelaCadastro.firstChild);
+    }
+    // Reexibe todas as pessoas após a remoção
+    for (let i = 0; i < pessoas.length; i++) {
+        mostraPessoa(pessoas[i], i);
+    }
 }
