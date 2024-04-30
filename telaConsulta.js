811787e76eceadf08cc4ac874440eebb729bbb5e
NickolasKnihs <160547042+NickolasKnihs@users.noreply.github.com>
2024-04-30 18:40:37 -0300

    Add files via upload

--- telaConsulta.js
+++ telaConsulta.js
@@ -6,30 +6,27 @@ for (let i = 0; i < pessoas.length; i++) {
     mostraPessoa(pessoas[i], i); // Adicionando o índice como segundo parâmetro
 }
 
-function mostraPessoa(parametro, indice) { // Recebendo também o índice
-    const paragrafo = document.createElement('p');
-    paragrafo.innerHTML += `
-        <span> Nome: ${parametro.nome}</span>
-        <br>
-        <img src="${parametro.foto}" alt="Foto de ${parametro.nome}" class="foto-pequena">
-        <br>
-        <span> Idade: ${parametro.idade}</span>
-        <br>
-        <span> CPF: ${parametro.cpf}</span>
-        <br>
-        <span> CNPJ: ${parametro.cnpj}</span>
-        <br>
-        <span> E-mail: ${parametro.email}</span>
-        <br>
-        <span> Gênero: ${parametro.genero}</span>
-        <br>
-        <div class="buttons-container">
-            <button onclick="removerUsuario(${indice});">REMOVER</button> <!-- Chama a função removerUsuario passando o índice -->
-            <button onclick="window.location.href = './telaApresentacao.html';" target="_blank">EDITAR</button>
-        </div>
+function mostraPessoa(parametro, indice) {
+    const linha = document.createElement('tr');
+    // Adiciona classe 'linha-impar' ou 'linha-par' dependendo do índice
+    linha.classList.add(indice % 2 === 0 ? 'linha-impar' : 'linha-par');
+    linha.innerHTML += `
+        <td><span> Nome: ${parametro.nome}</span></td>
+        <td><img src="${parametro.foto}" alt="Foto de ${parametro.nome}" class="foto-pequena"></td>
+        <td><span> Idade: ${parametro.idade}</span></td>
+        <td><span> CPF: ${parametro.cpf}</span></td>
+        <td><span> CNPJ: ${parametro.cnpj}</span></td>
+        <td><span> E-mail: ${parametro.email}</span></td>
+        <td><span> Gênero: ${parametro.genero}</span></td>
+        <td class="buttons-container">
+            <button onclick="removerUsuario(${indice});">REMOVER</button>
+            <button onclick="editarUsuario(${indice});">EDITAR</button>
+        </td>
     `;
-    tabelaCadastro.append(paragrafo);
+    tabelaCadastro.append(linha);
 }
+
+
 
 function removerUsuario(indice) {
     pessoas.splice(indice, 1); // Remove o usuário da matriz pelo índice
